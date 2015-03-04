6ee1014a86def5360432deb94f339c28fb0cffaa
Samuel Gaist <samuel.gaist@edeltech.ch>
2015-03-04 20:16:50 +0100

    Fix kmap2qmap build on OS X
    Fix kmap2qmap build on OS X
    
    Currently kmap2qmap fails to build on OS X (clang) This patch aims to
    fix this.
    
    Change-Id: I61c985dc7ad1f2486368c39aa976599d274942ab
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

--- tools/kmap2qmap/main.cpp
+++ tools/kmap2qmap/main.cpp
@@ -385,9 +385,11 @@ static const int symbol_synonyms_size = sizeof(symbol_synonyms)/sizeof(symbol_sy
 
 // makes the generated array in --header mode a bit more human readable
 QT_BEGIN_NAMESPACE
-static bool operator<(const QWSKeyboard::Mapping &m1, const QWSKeyboard::Mapping &m2)
-{
-    return m1.keycode != m2.keycode ? m1.keycode < m2.keycode : m1.modifiers < m2.modifiers;
+namespace QWSKeyboard {
+    static bool operator<(const Mapping &m1, const Mapping &m2)
+    {
+        return m1.keycode != m2.keycode ? m1.keycode < m2.keycode : m1.modifiers < m2.modifiers;
+    }
 }
 QT_END_NAMESPACE
 
