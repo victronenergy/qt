5aee9dabb55eb93c31b8f81c9fbf1481a4eb0c96
Jeroen Hofstee <jhofstee@victronenergy.com>
2020-05-06 12:51:33 +0200

    add .model to QDeclarativeVisualDataModelData
    This allows to get the QDeclarativeVisualDataModel itself back in the
    delegate. The QDeclarativeVisualDataModelData seems to lack a qml
    context.

--- src/declarative/graphicsitems/qdeclarativevisualitemmodel.cpp
+++ src/declarative/graphicsitems/qdeclarativevisualitemmodel.cpp
@@ -503,6 +503,8 @@ public:
     ~QDeclarativeVisualDataModelData();
 
     Q_PROPERTY(int index READ index NOTIFY indexChanged)
+    Q_PROPERTY(QDeclarativeVisualDataModel * model READ model CONSTANT)
+
     int index() const;
     void setIndex(int index);
 
@@ -518,6 +520,8 @@ public:
     }
 
     void ensureProperties();
+
+    Q_INVOKABLE QDeclarativeVisualDataModel *model() { return m_model; }
 
 Q_SIGNALS:
     void indexChanged();
